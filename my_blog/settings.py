153c329dda4ee4b6163824db6ebb0aff8dc98f88
Owen Jones <owenmjones1@gmail.com>
2022-01-23 15:09:15 +0100

    try to serve static files

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT = BASE_DIR / 'static_files'
+STATIC_ROOT = 'static_files'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
