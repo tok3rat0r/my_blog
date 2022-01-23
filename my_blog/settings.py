d6daf5b9295103e9ebcecd58b465af7b2f75fb71
Owen Jones <owenmjones1@gmail.com>
2022-01-23 11:11:11 +0100

    Remaining changes for deployment

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from os import getenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,10 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-13=egx$2l^5+i)(%!m*cci&b0--47u3*(-v0%e64y0w#1n23*c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['new-django-env.eba-fb9g7i9p.eu-west-3.elasticbeanstalk.com']
 
 # Application definition
 
@@ -117,7 +117,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_ROOT = BASE_DIR / 'static_files'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
